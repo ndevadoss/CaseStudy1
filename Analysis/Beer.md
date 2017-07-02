e7c81445723b09de05b53b0d6e4140e1c9f25f9d
ndevadoss <itznityadoss@gmail.com>
2017-07-02 13:49:05 -0500

    Added more comments for the code

--- Analysis/Beer.md
+++ Analysis/Beer.md
@@ -29,12 +29,14 @@ July 1, 2017
 ### Based on this analysis we will discover various information which could be used for marketing decisions , like ; which state has most number of breweries ; which state has lowest ; Is there a relation between IBU and ABV; If so what is the strength ; Are there any outliers.
 
 ## In order to perform the study we will do the following steps which will also answer the questions :
-### Load the required libraries to perform the study.
+### Load the required libraries to perform the study.We will need readr for file reading and ggplot2 for br plot and scatter plot
 
 
 
 ## Reading Files 
-
+#### read.csv will read the csv file , header is set to true and all blanks will be read as NA
+#### This will help for easy data cleanup
+#### str function will display the field names and their datatype. It also displays the number of observations and the class of the variable
 
 ```r
 Beers <- read.csv("C:/Nithya/MSDS/Term1/6306_Doing_DS/Week7/CaseStudy/Download/Beers.csv",header=T,na.strings="")
@@ -70,6 +72,8 @@ str(Breweries)
 ## Answers to the 7 Questions 
 
 # 1. Count the number of breweries by state
+#### Base package has aggregate function which can be used to count the rows based on a group by variable
+#### group by here is on state and we will get the count of brew id . this can be any other variable in the dataframe
 
 ```r
 aggregate(cbind(count = Breweries$Brew_ID) ~ Breweries$State, 
@@ -137,6 +141,11 @@ aggregate(cbind(count = Breweries$Brew_ID) ~ Breweries$State,
  
 
 # 2. Merging Beers and Breweries data
+#### merge function is used to merge 2 dataframes Beer and Breweries with a joining column which is brewid. 
+#### Since the common column name is different in both dataframes we can use by.x and by.y argument to define the joining column name
+#### str function displays the class of the variable and the total number of observations
+#### head function displays the first 6 observations by default
+#### tail function displays the last 6 observations by default
 
 ```r
 merge_df<-merge(Breweries,Beers,by.x = "Brew_ID" , by.y = "Brewery_id")
@@ -160,7 +169,7 @@ str(merge_df)
 
 ```r
 ## **************
-##First 6 records
+## First 6 records
 ## **************
 head(merge_df)
 ```
@@ -184,7 +193,7 @@ head(merge_df)
 
 ```r
 ## *************
-##Last 6 records
+## Last 6 records
 ## *************
 tail(merge_df)
 ```
@@ -216,6 +225,8 @@ tail(merge_df)
 ### After merging we have 2410 obs. of  10 variables. This dataset is available in Data Directory along with the sample data. Filename : merge.csv
 
 # 3. Reporting # of NA's
+#### is.na function is used to find the observations which is NA which is the result of merge or the blanks which were converted to NA when we read the file
+#### colsums will get the total count of the observations if is.na returns true
 
 ```r
 colSums(is.na(merge_df))
@@ -230,6 +241,9 @@ colSums(is.na(merge_df))
 ### Based on the result ,we have 62 NA in the column ABV , 1005 NA in the column IBU and 5 NA in the column Style. 
 
 # 4.Median alcohol content for each state
+#### aggregate function is used to get the median value of ABV group by state.na.rm is set to TRUE to remove the NA observations and then get the median value
+#### colnames sets the column names of the derived dataframe to a meaningful literal
+#### Summary function will give the 5 point summary statistics of the variable given as argument
 
 ```r
 ABV_ST<-aggregate(merge_df$ABV ~ merge_df$State, data = merge_df, median,na.rm=TRUE)
@@ -307,6 +321,9 @@ summary(ABV_ST)
 ##  (Other):45
 ```
 ## Bar Chart to show Median ABV in various states
+#### ggplot function is used to get the bar chart where x axis is the state and y axis is Median ABV.ggtitle gives the heading/title to the bar chart
+#### xlab and ylab is the xlabel and ylabel to the x&y axis repectively
+#### geom_bar creates the bar plot and the lining is yellow.The xaxis texts were colliding and hence adjusted the text to flip to 90deg
 
 ```r
 p <-ggplot(ABV_ST, aes(ABV_ST$State, ABV_ST$ABV))
@@ -319,6 +336,9 @@ p +geom_bar(stat = "identity",colour="YELLOW") + theme(axis.text.x=element_text(
 ### There is little variation across all the states in median Alcohol  by volume. It is almost equal. This can be seen from the bar chart and also by the summary statistics. The minimum is 0.04 for the state of UT and maximum is 0.06 for the state of DC,KY,NV,MI,NM,OK,CT,CO,SD,WV  
 
 ## Median International bitteness Unit for each state
+#### aggregate function is used to get the median value of IBU group by state.na.rm is set to TRUE to remove the NA observations and then get the median value
+#### colnames function sets the column names of the derived dataframe to a meaningful literal
+#### Summary function will give the 5 point summary statistics of the variable given as argument
 
 ```r
 IBU_ST<-aggregate(merge_df$IBU ~ merge_df$State, data = merge_df, median,na.rm=TRUE)
@@ -395,6 +415,9 @@ summary(IBU_ST)
 ##  (Other):44
 ```
 ## Bar Chart to show median IBU in various states
+#### ggplot function is used to get the bar chart where x axis is the state and y axis is Median IBU.ggtitle gives the heading/title to the bar chart
+#### xlab and ylab is the xlabel and ylabel to the x&y axis repectively
+#### geom_bar creates the bar plot and the lining is yellow.The xaxis texts were colliding and hence adjusted the text to flip to 90deg
 
 ```r
 q <-ggplot(IBU_ST, aes(IBU_ST$State, IBU_ST$IBU))
@@ -407,6 +430,9 @@ q +geom_bar(stat = "identity",colour="YELLOW") + theme(axis.text.x=element_text(
 ### There is variation across the states in median IBU. This can be seen from the bar chart and also by the summary statistics. The minimum is 19 for the state of WI and maximum is 61 for the state of ME 
 
 # 5. State having max alcoholic beer
+#### which.max function gives the maximum value of the variable passed as argument.
+#### str function find the column positions to get the column position
+#### Based on the column position,print state and ABV from the vector or df
 
 ```r
 alc<-merge_df[which.max(merge_df$ABV),]
@@ -420,6 +446,9 @@ alc[c(4,7)]
 ### State of Colorado (CO) with ABV = 0.128
 
 ## State with Most bitter beer
+#### which.max function gives the maximum value of the variable passed as argument.
+#### prints the entire row for the max value of IBU
+#### Based on the column position,print state and IBU from the vector/df
 
 ```r
 bitter<-merge_df[which.max(merge_df$IBU),]
@@ -433,6 +462,8 @@ bitter[c(4,8)]
 ### State of Oregon (OR) with IBU = 138
 
 # 6. Summary Statistics for ABV
+#### summary function gives the 5 point summary stats. We see that there are some NAs.
+#### Since there are some NAs , use na.omit to remove the NAs and print the sumamry stats again
 
 ```r
 summary(merge_df$ABV)
@@ -456,6 +487,11 @@ summary(abv_n)
 ### From the 5 point summary statistics, irrespective of the state , the minimum value is 0.00100 , Max val is 0.12800 ,Mean is 0.05977 , median is 0.05600 , Q1 is 0.05000 and Q3 is 0.06700
 
 # 7. Relationship between Bitterness and Alcoholic content
+#### na.omit removes the NA observations before plotting to avoid misleading results.
+#### ggplot function is used to get the bar chart where x axis is the ABV and y axis is IBU.ggtitle gives the heading/title to the bar chart
+#### xlab and ylab is the xlabel and ylabel to the x&y axis repectively
+#### geom_point creates the scatter plot and the color chosen is Red. geom_smooth is used to draw a line along the observation points .
+#### Since the method chosen is lm (linear method) it draws a 45deg linear line across the plot to show the linear regression. Color of the line is chosen to be BLUE
 
 ```r
 merge_df1 <- na.omit(merge_df)
--- Analysis/BeerAnalysis.R
+++ Analysis/BeerAnalysis.R
@@ -1,56 +1,22 @@
----
-title: "BeerStudy"
-author: "Nithya Devadoss"
-date: "July 1, 2017"
-output: 
-  html_document: 
-    keep_md: yes
----
-# Introduction:
-### This case study is to analyze everything about Beers and Breweries across United States. We have the sample data collected on Beers and Breweries and is available in the Data directory inside the CaseStudy Repository in github.File count as following:
-
-# Data Dictionary:
-## Beers data file has 2410 observations and 7 columns: 
-
-## Beers.csv: 
-### Name: Name of the beer. 
-### Beer ID: Unique identiﬁer of the beer
-### ABV: Alcohol by volume of the beer. 
-### IBU: International Bitterness Units of the beer. 
-### Brewery ID: Brewery id associated with the beer. 
-### Style: Style of the beer. 
-### Ounces: Ounces of beer.
-
-## Breweries data file has 558 Observations and 4 columns
-
-## Breweries.csv: 
-### Brew ID: Unique identiﬁer of the brewery. 
-### Name: Name of the brewery. 
-### City: City where the brewery is located. 
-### State: State where the brewery is located.
-
-
-# Objective:
-### Based on this analysis we will discover various information which could be used for marketing decisions , like ; which state has most number of breweries ; which state has lowest ; Is there a relation between IBU and ABV; If so what is the strength ; Are there any outliers.
-
 ## In order to perform the study we will do the following steps which will also answer the questions :
-### Load the required libraries to perform the study.We will need readr for file reading and ggplot2 for br plot and scatter plot
-```{r,loading libraries, echo=FALSE }
+### Load the required libraries to perform the study.We will need readr for file reading 
+### and ggplot2 for br plot and scatter plot
+
 library(readr)
 library(ggplot2)
-```
 
 
 ## Reading Files 
 #### read.csv will read the csv file , header is set to true and all blanks will be read as NA
 #### This will help for easy data cleanup
-#### str function will display the field names and their datatype. It also displays the number of observations and the class of the variable
-```{r, Read csv files, echo=TRUE}
+
 Beers <- read.csv("C:/Nithya/MSDS/Term1/6306_Doing_DS/Week7/CaseStudy/Download/Beers.csv",header=T,na.strings="")
 Breweries <- read.csv("C:/Nithya/MSDS/Term1/6306_Doing_DS/Week7/CaseStudy/Download/Breweries.csv",header=T,na.strings="")
+#### str function will display the field names and their datatype. It also displays the number of observations and the class of the variable
+
 str(Beers)
 str(Breweries)
-```
+
 ## Beers data file has 2410 observations and 7 columns
 ## Breweries data file has 558 Observations and 4 columns
 
@@ -59,136 +25,121 @@ str(Breweries)
 # 1. Count the number of breweries by state
 #### Base package has aggregate function which can be used to count the rows based on a group by variable
 #### group by here is on state and we will get the count of brew id . this can be any other variable in the dataframe
-```{r  , Count of breweries by state  }
 aggregate(cbind(count = Breweries$Brew_ID) ~ Breweries$State, 
           data = Breweries, 
           FUN = function(x){NROW(x)})
-```
+
 
 ### There are 47 breweries in CO , followed by CA with 39 Breweries
 ### On the contrary , there is only one Brewery in DC,ND,SD,WV
- 
+
 
 # 2. Merging Beers and Breweries data
 #### merge function is used to merge 2 dataframes Beer and Breweries with a joining column which is brewid. 
 #### Since the common column name is different in both dataframes we can use by.x and by.y argument to define the joining column name
-#### str function displays the class of the variable and the total number of observations
-#### head function displays the first 6 observations by default
-#### tail function displays the last 6 observations by default
-```{r  , Merge Data  }
 merge_df<-merge(Breweries,Beers,by.x = "Brew_ID" , by.y = "Brewery_id")
 ## Number of observations after data merging
+#### str function displays the class of the variable and the total number of observations
 str(merge_df)
 ## **************
 ## First 6 records
 ## **************
+#### head function displays the first 6 observations by default
 head(merge_df)
 ## *************
 ## Last 6 records
 ## *************
+#### tail function displays the last 6 observations by default
 tail(merge_df)
-```
+
 
 ### After merging we have 2410 obs. of  10 variables. This dataset is available in Data Directory along with the sample data. Filename : merge.csv
-
+write.csv(merge_df, file = "C:/Nithya/MSDS/Term1/6306_Doing_DS/Week7/CaseStudy/Download/merge.csv", row.names = TRUE)
+#### write function exports the dataframe to a file format and to the location specified
 # 3. Reporting # of NA's
 #### is.na function is used to find the observations which is NA which is the result of merge or the blanks which were converted to NA when we read the file
 #### colsums will get the total count of the observations if is.na returns true
-```{r  , count of NA  }
 colSums(is.na(merge_df))
-```
+
 ### Based on the result ,we have 62 NA in the column ABV , 1005 NA in the column IBU and 5 NA in the column Style. 
 
 # 4.Median alcohol content for each state
 #### aggregate function is used to get the median value of ABV group by state.na.rm is set to TRUE to remove the NA observations and then get the median value
-#### colnames sets the column names of the derived dataframe to a meaningful literal
-#### Summary function will give the 5 point summary statistics of the variable given as argument
-```{r  , Median ABV  }
 ABV_ST<-aggregate(merge_df$ABV ~ merge_df$State, data = merge_df, median,na.rm=TRUE)
+#### Setting the column names of the derived dataframe to a meaningful literal
 colnames(ABV_ST) <- c("State","ABV") 
 ABV_ST
+#### Summary function will give the 5 point summary statistics of the variable given as argument
 summary(ABV_ST)
-```
+
 ## Bar Chart to show Median ABV in various states
 #### ggplot function is used to get the bar chart where x axis is the state and y axis is Median ABV.ggtitle gives the heading/title to the bar chart
 #### xlab and ylab is the xlabel and ylabel to the x&y axis repectively
 #### geom_bar creates the bar plot and the lining is yellow.The xaxis texts were colliding and hence adjusted the text to flip to 90deg
-```{r , Bar plot ABV}
 p <-ggplot(ABV_ST, aes(ABV_ST$State, ABV_ST$ABV))
 p +geom_bar(stat = "identity",colour="YELLOW") + theme(axis.text.x=element_text(angle=90,hjust=1)) + xlab("State") + ylab("Median ABV") + ggtitle("ABV Vs State") 
-```
+
 
 ## Bar chart Inference: 
 ### There is little variation across all the states in median Alcohol  by volume. It is almost equal. This can be seen from the bar chart and also by the summary statistics. The minimum is 0.04 for the state of UT and maximum is 0.06 for the state of DC,KY,NV,MI,NM,OK,CT,CO,SD,WV  
 
 ## Median International bitteness Unit for each state
 #### aggregate function is used to get the median value of IBU group by state.na.rm is set to TRUE to remove the NA observations and then get the median value
-#### colnames function sets the column names of the derived dataframe to a meaningful literal
-#### Summary function will give the 5 point summary statistics of the variable given as argument
-```{r  , Median IBU  }
 IBU_ST<-aggregate(merge_df$IBU ~ merge_df$State, data = merge_df, median,na.rm=TRUE)
+#### Setting the column names of the derived dataframe to a meaningful literal
 colnames(IBU_ST) <- c("State","IBU")
 IBU_ST
+#### Summary function will give the 5 point summary statistics of the variable given as argument
 summary(IBU_ST)
-```
+
 ## Bar Chart to show median IBU in various states
 #### ggplot function is used to get the bar chart where x axis is the state and y axis is Median IBU.ggtitle gives the heading/title to the bar chart
 #### xlab and ylab is the xlabel and ylabel to the x&y axis repectively
 #### geom_bar creates the bar plot and the lining is yellow.The xaxis texts were colliding and hence adjusted the text to flip to 90deg
-```{r , Bar plot IBU}
 q <-ggplot(IBU_ST, aes(IBU_ST$State, IBU_ST$IBU))
 q +geom_bar(stat = "identity",colour="YELLOW") + theme(axis.text.x=element_text(angle=90,hjust=1)) + xlab("State") + ylab("Median IBU") + ggtitle("IBU Vs State") 
-```
+
 
 ## Bar chart Inference: 
 ### There is variation across the states in median IBU. This can be seen from the bar chart and also by the summary statistics. The minimum is 19 for the state of WI and maximum is 61 for the state of ME 
 
 # 5. State having max alcoholic beer
 #### which.max function gives the maximum value of the variable passed as argument.
-#### str function find the column positions to get the column position
-#### Based on the column position,print state and ABV from the vector or df
-```{r  , State with max alcohol }
 alc<-merge_df[which.max(merge_df$ABV),]
+#### prints the entire row for the max value of ABV
+alc
+#### find the column positions to get the column position
+str(alc)
+#### Based on the column position,print state and ABV
 alc[c(4,7)]
-```
+
 ### State of Colorado (CO) with ABV = 0.128
 
 ## State with Most bitter beer
 #### which.max function gives the maximum value of the variable passed as argument.
-#### prints the entire row for the max value of IBU
-#### Based on the column position,print state and IBU from the vector/df
-```{r  , State with most bitter beer  }
 bitter<-merge_df[which.max(merge_df$IBU),]
+#### prints the entire row for the max value of IBU
+bitter
+#### Based on the column position,print state and IBU
 bitter[c(4,8)]
-```
+
 ### State of Oregon (OR) with IBU = 138
 
 # 6. Summary Statistics for ABV
 #### summary function gives the 5 point summary stats. We see that there are some NAs.
-#### Since there are some NAs , use na.omit to remove the NAs and print the sumamry stats again
-```{r , Summ stat for ABV}
 summary(merge_df$ABV)
 ## After removing NAs
+#### Since there are some NAs , use na.omit to remove the NAs and print the sumamry stats again
 abv_n<-na.omit(merge_df$ABV)
 summary(abv_n)
-```
-### From the 5 point summary statistics, irrespective of the state , the minimum value is 0.00100 , Max val is 0.12800 ,Mean is 0.05977 , median is 0.05600 , Q1 is 0.05000 and Q3 is 0.06700
+
 
 # 7. Relationship between Bitterness and Alcoholic content
 #### na.omit removes the NA observations before plotting to avoid misleading results.
+merge_df1 <- na.omit(merge_df)
 #### ggplot function is used to get the bar chart where x axis is the ABV and y axis is IBU.ggtitle gives the heading/title to the bar chart
 #### xlab and ylab is the xlabel and ylabel to the x&y axis repectively
 #### geom_point creates the scatter plot and the color chosen is Red. geom_smooth is used to draw a line along the observation points .
 #### Since the method chosen is lm (linear method) it draws a 45deg linear line across the plot to show the linear regression. Color of the line is chosen to be BLUE
-```{r , Scatter plot}
-merge_df1 <- na.omit(merge_df)
 ggplot(merge_df1 ,aes(x=merge_df1$ABV, y=merge_df1$IBU)) + geom_point(shape=20, color="RED") +geom_smooth(method='lm',color="BLUE") + xlab("Alcohol By Vol") + ylab("International Bitterness Unit") +
   ggtitle("Bitterness Vs Alcohol Content") 
-```
-
-## Scatter Plot Inference
-### From the scatter plot we see that the relationship is mildly linear. There are few outliers with ABV=0.06 and IBU >110 ,  ABV=0.092 and IBU ~= 1.We can run a correlation test before and after removing the outliers to prove statistically.The data shows that most beers are with ABV around .05 has bitterness level (IBU) approximately around 50
-
-# Conclusion:
-
-### Here are the key takeaways from the above analysis based on the sample we have on Beers and Breweries - Maximum number of breweries is in CO (47) followed by CA(39).  There is only one brewery in DC,ND,SD,WV. There could be various factors which could affect this figure , for example State laws and tourism. State with most alcohol content is Colorado(CO) with ABV = 0.128 and the state with most bitter beer is Oregon (OR) with IBU = 138. The median alcohol content is around 0.05 which is approximately the same across various states. The minimum is in the state of UT. The bitterness level which is measured by IBU varies across various states and has a mildly linear correlation to the alcohol content. From the plots we also find that there are a few outliers which could affect this correlation statistically. Further research should be done with and without outliers and publish the study results. 
